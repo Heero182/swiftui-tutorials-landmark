d3d4575ecf15cbec7a1a6dc502968d8377df8bf0
Heider Bernardes <heiderbs@gmail.com>
2023-04-29 15:43:45 -0300

    Initial Commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by HEIDER B SILVEIRA on 27/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
